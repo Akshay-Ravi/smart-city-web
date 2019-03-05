f0c8a466534b30b717381538f973a5f426270afe
Gautham Kumar <heygautham@gmail.com>
2019-03-05 13:24:19 +0530

    Added light and shadows

--- game.js
+++ game.js
@@ -3,21 +3,57 @@ var Game = /** @class */ (function () {
         // Create canvas and engine.
         this._canvas = document.getElementById(canvasElement);
         this._engine = new BABYLON.Engine(this._canvas, true);
+        // Initialize properties
+        this._directionalLight = {
+            light: undefined,
+            position: new BABYLON.Vector3(0, 10, 0),
+            direction: new BABYLON.Vector3(-0.5, -1, -0.5),
+            intensity: 0.6
+        };
+        this._ambientLight = {
+            light: undefined,
+            direction: new BABYLON.Vector3(0, 1, 0),
+            intensity: 0.3
+        };
     }
     Game.prototype.createScene = function () {
         // Create a basic BJS Scene object.
         this._scene = new BABYLON.Scene(this._engine);
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI / 4, Math.PI / 4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
-        // Target the camera to scene origin.
-        this._camera.setTarget(BABYLON.Vector3.Zero());
-        // Attach the camera to the canvas.
-        this._camera.attachControl(this._canvas, false);
-        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
+        this._scene.clearColor = new BABYLON.Color4(0, 0, 0, 1); // Set background color to black
+        // Create the camera
+        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI / 3, Math.PI / 4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
+        this._camera.attachControl(this._canvas, false); // Attach the camera to the canvas to allow mouse movement
+        // Create all light necessary for the scene
+        this.createLight();
         // Create a built-in "ground" shape.
         var ground = BABYLON.MeshBuilder.CreateGround('ground1', { width: 12, height: 12, subdivisions: 1 }, this._scene);
-        ground.position.set(-2, 0, -2);
+        ground.position.set(0, 0, 0);
+        // Create a box now to play around with shadows (This will be removed once we create cars and buildings)
+        var box = BABYLON.MeshBuilder.CreateBox("box", {
+            size: 4
+        }, this._scene);
+        box.position.set(0, 3, 0);
+        this.generateShadows(ground, box);
+    };
+    Game.prototype.createLight = function () {
+        // Directional Light is used to generate shadows
+        this._directionalLight.light = new BABYLON.DirectionalLight("sunlight", this._directionalLight.direction, this._scene);
+        this._directionalLight.light.position = this._directionalLight.position;
+        this._directionalLight.light.intensity = this._directionalLight.intensity;
+        // Ambient Light is of type Hemispheric Light
+        this._ambientLight.light = new BABYLON.HemisphericLight("ambientlight", this._ambientLight.direction, this._scene);
+        this._ambientLight.light.intensity = this._ambientLight.intensity;
+    };
+    Game.prototype.generateShadows = function (ground) {
+        var items = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            items[_i - 1] = arguments[_i];
+        }
+        var shadowGenerator = new BABYLON.ShadowGenerator(512, this._directionalLight.light);
+        items.forEach(function (item) {
+            shadowGenerator.getShadowMap().renderList.push(item);
+        });
+        ground.receiveShadows = true;
     };
     Game.prototype.doRender = function () {
         var _this = this;
--- game.ts
+++ game.ts
@@ -3,35 +3,81 @@ class Game {
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
     private _camera: BABYLON.ArcRotateCamera;
-    private _light: BABYLON.Light;
+    private _directionalLight: {
+        light: BABYLON.DirectionalLight,
+        position: BABYLON.Vector3,
+        direction: BABYLON.Vector3,
+        intensity: number
+    }
+    private _ambientLight: {
+        light: BABYLON.HemisphericLight,
+        direction: BABYLON.Vector3,
+        intensity: number
+    }
 
     constructor(canvasElement : string) {
         // Create canvas and engine.
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
         this._engine = new BABYLON.Engine(this._canvas, true);
+
+        // Initialize properties
+        this._directionalLight = {
+            light: undefined,
+            position: new BABYLON.Vector3(0, 10, 0),
+            direction: new BABYLON.Vector3(-0.5, -1, -0.5),
+            intensity: 0.6
+        }
+        this._ambientLight = {
+            light: undefined,
+            direction: new BABYLON.Vector3(0, 1, 0),
+            intensity: 0.3
+        }
     }
 
     createScene() : void {
+
         // Create a basic BJS Scene object.
         this._scene = new BABYLON.Scene(this._engine);
+        this._scene.clearColor = new BABYLON.Color4(0,0,0,1); // Set background color to black
 
-        // Create a FreeCamera, and set its position to (x:0, y:5, z:-10).
-        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI/4, Math.PI/4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
+        // Create the camera
+        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI/3, Math.PI/4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
+        this._camera.attachControl(this._canvas, false); // Attach the camera to the canvas to allow mouse movement
 
-        // Target the camera to scene origin.
-        this._camera.setTarget(BABYLON.Vector3.Zero());
-
-        // Attach the camera to the canvas.
-        this._camera.attachControl(this._canvas, false);
-
-        // Create a basic light, aiming 0,1,0 - meaning, to the sky.
-        this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
+        // Create all light necessary for the scene
+        this.createLight();
 
         // Create a built-in "ground" shape.
         let ground = BABYLON.MeshBuilder.CreateGround('ground1', {width: 12, height: 12, subdivisions: 1}, this._scene);
+        ground.position.set(0,0,0);
 
-        ground.position.set(-2,0,-2);
+        // Create a box now to play around with shadows (This will be removed once we create cars and buildings)
+        let box = BABYLON.MeshBuilder.CreateBox("box", {
+            size: 4
+        }, this._scene);
+        box.position.set(0,3,0)
 
+        this.generateShadows(ground, box);
+    }
+
+    createLight(): void {
+        // Directional Light is used to generate shadows
+        this._directionalLight.light = new BABYLON.DirectionalLight("sunlight", this._directionalLight.direction, this._scene);
+        this._directionalLight.light.position = this._directionalLight.position;
+        this._directionalLight.light.intensity = this._directionalLight.intensity;
+        
+        // Ambient Light is of type Hemispheric Light
+        this._ambientLight.light = new BABYLON.HemisphericLight("ambientlight", this._ambientLight.direction, this._scene);
+        this._ambientLight.light.intensity = this._ambientLight.intensity;
+    }
+
+    generateShadows(ground: BABYLON.Mesh, ...items: BABYLON.AbstractMesh[]): void {
+        let shadowGenerator = new BABYLON.ShadowGenerator(512, this._directionalLight.light);
+        items.forEach(item => {
+            shadowGenerator.getShadowMap().renderList.push(item)
+        });
+
+        ground.receiveShadows = true;
     }
 
     doRender() : void {
