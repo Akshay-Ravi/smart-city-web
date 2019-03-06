bf00fa7faaf3e77ad8051c5c001d009f50f1be4e
Gautham Kumar <heygautham@gmail.com>
2019-03-06 12:39:38 +0530

    Added roads and grass textures

--- game.js
+++ game.js
@@ -15,25 +15,58 @@ var Game = /** @class */ (function () {
             direction: new BABYLON.Vector3(0, 1, 0),
             intensity: 0.3
         };
+        this._verticalRoads = [];
+        this._horizontalRoads = [];
     }
     Game.prototype.createScene = function () {
         // Create a basic BJS Scene object.
         this._scene = new BABYLON.Scene(this._engine);
         this._scene.clearColor = new BABYLON.Color4(0, 0, 0, 1); // Set background color to black
         // Create the camera
-        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI / 3, Math.PI / 4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
+        this._camera = new BABYLON.ArcRotateCamera('maincamera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(30), 18, new BABYLON.Vector3(0, 0, 0), this._scene);
         this._camera.attachControl(this._canvas, false); // Attach the camera to the canvas to allow mouse movement
         // Create all light necessary for the scene
         this.createLight();
         // Create a built-in "ground" shape.
-        var ground = BABYLON.MeshBuilder.CreateGround('ground1', { width: 12, height: 12, subdivisions: 1 }, this._scene);
+        var ground = BABYLON.MeshBuilder.CreateGround('ground1', { width: 20, height: 12, subdivisions: 1 }, this._scene);
         ground.position.set(0, 0, 0);
-        // Create a box now to play around with shadows (This will be removed once we create cars and buildings)
-        var box = BABYLON.MeshBuilder.CreateBox("box", {
-            size: 4
-        }, this._scene);
-        box.position.set(0, 3, 0);
-        this.generateShadows(ground, box);
+        // Give ground a grass texture
+        var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", this._scene);
+        var groundTexture = new BABYLON.Texture("images/grass.jpg", this._scene);
+        groundTexture.uScale = 20;
+        groundTexture.vScale = 12;
+        groundMaterial.diffuseTexture = groundTexture;
+        ground.material = groundMaterial;
+        // Create the road texture for assigning to the road meshes later
+        var roadMaterial = new BABYLON.StandardMaterial("roadMaterial", this._scene);
+        roadMaterial.diffuseTexture = new BABYLON.Texture("images/road.png", this._scene);
+        // CREATE ALL ROADS
+        var road1 = BABYLON.MeshBuilder.CreatePlane("road1", { width: 2, height: 12 }, this._scene);
+        road1.position.set(0, 0.01, 0);
+        this._verticalRoads.push(road1);
+        var road2 = BABYLON.MeshBuilder.CreatePlane("road1", { width: 2, height: 12 }, this._scene);
+        road2.position.set(-6, 0.01, 0);
+        this._verticalRoads.push(road2);
+        var road3 = BABYLON.MeshBuilder.CreatePlane("road1", { width: 2, height: 12 }, this._scene);
+        road3.position.set(6, 0.01, 0);
+        this._verticalRoads.push(road3);
+        var road4 = BABYLON.MeshBuilder.CreatePlane("road1", { width: 2, height: 20 }, this._scene);
+        road4.position.set(0, 0.02, -3);
+        this._horizontalRoads.push(road4);
+        var road5 = BABYLON.MeshBuilder.CreatePlane("road1", { width: 2, height: 20 }, this._scene);
+        road5.position.set(0, 0.02, 3);
+        this._horizontalRoads.push(road5);
+        this._verticalRoads.forEach(function (road) {
+            road.material = roadMaterial;
+            road.rotate(new BABYLON.Vector3(0, 1, 0), BABYLON.Tools.ToRadians(180));
+            road.rotate(new BABYLON.Vector3(1, 0, 0), BABYLON.Tools.ToRadians(90));
+        });
+        this._horizontalRoads.forEach(function (road) {
+            road.material = roadMaterial;
+            road.rotate(new BABYLON.Vector3(0, 1, 0), BABYLON.Tools.ToRadians(270));
+            road.rotate(new BABYLON.Vector3(1, 0, 0), BABYLON.Tools.ToRadians(90));
+        });
+        this.generateShadows(ground);
     };
     Game.prototype.createLight = function () {
         // Directional Light is used to generate shadows
--- game.ts
+++ game.ts
@@ -14,6 +14,13 @@ class Game {
         direction: BABYLON.Vector3,
         intensity: number
     }
+    private _verticalRoads: Array<BABYLON.Mesh>
+    private _horizontalRoads: Array<BABYLON.Mesh>
+    private _verticalRoadHeight: number
+    private _horizontalRoadHeight: number
+    private _cityWidth: number
+    private _cityHeight: number
+    private _roadWidth: number
 
     constructor(canvasElement : string) {
         // Create canvas and engine.
@@ -32,6 +39,13 @@ class Game {
             direction: new BABYLON.Vector3(0, 1, 0),
             intensity: 0.3
         }
+        this._verticalRoads = [];
+        this._horizontalRoads = [];
+        this._verticalRoadHeight = 0.01;
+        this._horizontalRoadHeight = 0.02;
+        this._cityWidth = 20;
+        this._cityHeight = 12;
+        this._roadWidth = 2;
     }
 
     createScene() : void {
@@ -41,23 +55,62 @@ class Game {
         this._scene.clearColor = new BABYLON.Color4(0,0,0,1); // Set background color to black
 
         // Create the camera
-        this._camera = new BABYLON.ArcRotateCamera('maincamera', Math.PI/3, Math.PI/4, 15, new BABYLON.Vector3(0, 0, 0), this._scene);
+        this._camera = new BABYLON.ArcRotateCamera('maincamera', BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(30), 18, new BABYLON.Vector3(0, 0, 0), this._scene);
         this._camera.attachControl(this._canvas, false); // Attach the camera to the canvas to allow mouse movement
 
         // Create all light necessary for the scene
         this.createLight();
 
         // Create a built-in "ground" shape.
-        let ground = BABYLON.MeshBuilder.CreateGround('ground1', {width: 12, height: 12, subdivisions: 1}, this._scene);
+        let ground = BABYLON.MeshBuilder.CreateGround('ground1', {width: this._cityWidth, height: this._cityHeight, subdivisions: 1}, this._scene);
         ground.position.set(0,0,0);
+        
+        // Give ground a grass texture
+        var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", this._scene);
+        var groundTexture = new BABYLON.Texture("images/grass.jpg", this._scene);
+        groundTexture.uScale = this._cityWidth;
+        groundTexture.vScale = this._cityHeight;
+        groundMaterial.diffuseTexture = groundTexture;
+        ground.material = groundMaterial;
 
-        // Create a box now to play around with shadows (This will be removed once we create cars and buildings)
-        let box = BABYLON.MeshBuilder.CreateBox("box", {
-            size: 4
-        }, this._scene);
-        box.position.set(0,3,0)
+        // Create the road texture for assigning to the road meshes later
+        var roadMaterial = new BABYLON.StandardMaterial("roadMaterial", this._scene);
+        roadMaterial.diffuseTexture = new BABYLON.Texture("images/road.png", this._scene);
+        
+        // CREATE ALL ROADS
+        var road1 = BABYLON.MeshBuilder.CreatePlane("road1", {width: this._roadWidth, height: this._cityHeight}, this._scene);
+        road1.position.set(0,this._verticalRoadHeight,0);
+        this._verticalRoads.push(road1);
 
-        this.generateShadows(ground, box);
+        var road2 = BABYLON.MeshBuilder.CreatePlane("road2", {width: this._roadWidth, height: this._cityHeight}, this._scene);
+        road2.position.set(-6,this._verticalRoadHeight,0);
+        this._verticalRoads.push(road2);
+
+        var road3 = BABYLON.MeshBuilder.CreatePlane("road3", {width: this._roadWidth, height: this._cityHeight}, this._scene);
+        road3.position.set(6,this._verticalRoadHeight,0);
+        this._verticalRoads.push(road3);
+
+        var road4 = BABYLON.MeshBuilder.CreatePlane("road4", {width: this._roadWidth, height: this._cityWidth}, this._scene);
+        road4.position.set(0,this._horizontalRoadHeight,-3);
+        this._horizontalRoads.push(road4);
+
+        var road5 = BABYLON.MeshBuilder.CreatePlane("road5", {width: this._roadWidth, height: this._cityWidth}, this._scene);
+        road5.position.set(0,this._horizontalRoadHeight,3);
+        this._horizontalRoads.push(road5);
+        
+        this._verticalRoads.forEach(road => {
+            road.material = roadMaterial;
+            road.rotate(new BABYLON.Vector3(0,1,0), BABYLON.Tools.ToRadians(180));
+            road.rotate(new BABYLON.Vector3(1,0,0), BABYLON.Tools.ToRadians(90));
+        });
+
+        this._horizontalRoads.forEach(road => {
+            road.material = roadMaterial;
+            road.rotate(new BABYLON.Vector3(0,1,0), BABYLON.Tools.ToRadians(270));
+            road.rotate(new BABYLON.Vector3(1,0,0), BABYLON.Tools.ToRadians(90));
+        });
+
+        this.generateShadows(ground);
     }
 
     createLight(): void {
@@ -65,7 +118,7 @@ class Game {
         this._directionalLight.light = new BABYLON.DirectionalLight("sunlight", this._directionalLight.direction, this._scene);
         this._directionalLight.light.position = this._directionalLight.position;
         this._directionalLight.light.intensity = this._directionalLight.intensity;
-        
+
         // Ambient Light is of type Hemispheric Light
         this._ambientLight.light = new BABYLON.HemisphericLight("ambientlight", this._ambientLight.direction, this._scene);
         this._ambientLight.light.intensity = this._ambientLight.intensity;
